1466fdffe051cd6a2b8e7e35f335439e1a54d245
KasarapuVignesh <134696325+KasarapuVignesh@users.noreply.github.com>
2025-06-13 00:52:44 +0530

    Update gemini.py

--- llms/gemini.py
+++ llms/gemini.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("AIzaSyAAD-JH2-2aokSn1cpYCVnQHf-XS7NSV0A"))
+genai.configure(api_key=os.getenv("GEMINI-API-KEY"))
 
 model = genai.GenerativeModel("gemini-pro")
 
